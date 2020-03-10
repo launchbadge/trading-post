250f4ad7bc75d1942b7d58b8dbf04e61214b1b81
Tim <tim@launchbadge.com>
2020-03-09 17:19:51 -0700

    add gold to traderow

--- src/components/TradeRow.vue
+++ src/components/TradeRow.vue
@@ -24,7 +24,17 @@ export default Vue.defineComponent({
             <div class="TradeRow">
                 <Avatar publicKey={ props.trade.requestor.publicKey } />
                 <EmojiList class="TradeRow-emojiList" emoji={ props.trade.requestorEmoji } />
-                <Icon class="TradeRow-swapIcon" d={ mdiSwapHorizontalVariant } />
+                <div class="TradeRow-goldSwap">
+                    <div class="TradeRow-gold">
+                        <span class="TradeRow-goldAmount">{ props.trade.requestorGold.toFixed() }</span>
+                        <Icon class="TradeRow-goldIcon" d={ mdiCoinOutline } />
+                    </div>
+                    <Icon class="TradeRow-swapIcon" d={ mdiSwapHorizontalVariant } />
+                    <div class="TradeRow-goldMirror">
+                        <Icon class="TradeRow-goldIconMirror" d={ mdiCoinOutline } />
+                        <span>{ props.trade.requesteeGold.toFixed() }</span>
+                    </div>
+                </div>
                 <EmojiList class="TradeRow-emojiList" emoji={ props.trade.requesteeEmoji } />
                 <Avatar publicKey={ props.trade.requestee.publicKey } />
                 <StatusIcon accepted={ acceptStatus.value } />
@@ -41,7 +51,8 @@ export default Vue.defineComponent({
     color: var(--colorWhite);
     padding: 16px;
     border-radius: 2px;
-    display: flex;
+    display: inline-grid;
+    grid-template-columns: 1fr 5fr 230px 5fr 1fr 2fr;
     align-items: center;
 }
 
@@ -49,11 +60,44 @@ export default Vue.defineComponent({
     margin-inline: 16px;
 }
 
+.TradeRow-goldSwap {
+    display: inline-grid;
+    grid-template-columns: 2fr 1fr 2fr;
+    justify-content: center;
+}
+
 .TradeRow-swapIcon {
     height: 32px;
     fill: var(--colorWhite);
     margin-inline: 20px;
-    flex-grow: 1;
     opacity: 0.7;
+    justify-self: center;
 }
+
+.TradeRow-gold {
+    display: flex;
+    align-items: center;
+    justify-self: right;
+}
+
+.TradeRow-goldIcon {
+    fill: var(--colorSpandexGreen);
+    height: 24px;
+}
+
+.TradeRow-goldAmount {
+    margin-inline-end: 8px;
+}
+
+.TradeRow-goldMirror {
+    display: flex;
+    align-items: center;
+}
+
+.TradeRow-goldIconMirror {
+    fill: var(--colorSpandexGreen);
+    height: 24px;
+    margin-inline-end: 8px;
+}
+
 </style>
